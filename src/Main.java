d8d7701b48150d74b58cbdb7a4d9117cdb1cc7e5
Agustin <agus.menchon@gmail.com>
2024-05-24 18:33:46 -0300

    add tarea & procesador

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
 
             Servicios servicios = new Servicios("src/datasets/Procesadores.csv", "src/datasets/Tareas.csv");
-
         }
     }
